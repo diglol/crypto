acc52580b99618f0f439c3ca02313837f34c2360
RobX <robxyy@gmail.com>
2022-09-03 16:46:10 +0800

    Update dependencies.

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
   kotlin("multiplatform")
-  kotlin("plugin.allopen") version "1.6.21"
+  kotlin("plugin.allopen") version libs.versions.kotlin
   id("org.jetbrains.kotlinx.benchmark")
 }
 
