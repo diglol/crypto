f7c51ed49ad2229a2463a6972bf860a598ad2b2d
RobX <robxyy@gmail.com>
2022-09-10 13:19:14 +0800

    Exclude crypto polyfill

--- kdf/webpack.config.d/webpack.js
+++ kdf/webpack.config.d/webpack.js
@@ -1,5 +1,5 @@
 const path = require('path');
 config.resolve.modules.push(path.resolve(__dirname, 'node_modules'));
-config.resolve.fallback = {path: false, fs: false};
+config.resolve.fallback = {path: false, fs: false, crypto: false};
 config.module.noParse = /\.wasm$/;
 config.module.rules.push({test: /\.wasm$/, loader: 'base64-loader', type: 'javascript/auto'});
