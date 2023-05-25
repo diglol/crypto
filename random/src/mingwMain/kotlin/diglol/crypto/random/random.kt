80afa3493ea909ecbb7c7d42f69d79085a8b5b28
Rob X <robxyy@gmail.com>
2023-05-25 08:11:05 +0800

    Improving random module on Windows (#68)

--- random/src/mingwMain/kotlin/diglol/crypto/random/random.kt
+++ random/src/mingwMain/kotlin/diglol/crypto/random/random.kt
@@ -1,26 +1,14 @@
 package diglol.crypto.random
 
 import diglol.crypto.internal.emptyBytes
-import kotlinx.cinterop.CFunction
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.convert
-import kotlinx.cinterop.invoke
 import kotlinx.cinterop.reinterpret
 import kotlinx.cinterop.usePinned
-import platform.windows.BCRYPT_ALG_HANDLE
-import platform.windows.GetProcAddress
-import platform.windows.LoadLibraryA
-import platform.windows.PUCHAR
-import platform.windows.ULONG
+import platform.windows.BCRYPT_USE_SYSTEM_PREFERRED_RNG
+import platform.windows.BCryptGenRandom
 
 // https://docs.microsoft.com/zh-cn/windows/win32/api/bcrypt/nf-bcrypt-bcryptgenrandom
-private val bcrypt by lazy { LoadLibraryA("Bcrypt.dll") }
-private val bcryptGenRandom by lazy {
-  GetProcAddress(bcrypt, "BCryptGenRandom")
-    ?.reinterpret<CFunction<Function4<BCRYPT_ALG_HANDLE?, PUCHAR?, ULONG, ULONG, Int>>>()
-    ?: error("Can't find Bcrypt#BCryptGenRandom()")
-}
-
 actual fun nextInt(bound: Int): Int = commonNextInt(bound)
 
 actual fun nextBytes(size: Int): ByteArray {
@@ -29,7 +17,12 @@ actual fun nextBytes(size: Int): ByteArray {
   }
   return ByteArray(size).apply {
     usePinned {
-      bcryptGenRandom(null, it.addressOf(0).reinterpret(), this.size.convert(), 2.convert())
+      BCryptGenRandom(
+        null,
+        it.addressOf(0).reinterpret(),
+        this.size.convert(),
+        BCRYPT_USE_SYSTEM_PREFERRED_RNG
+      )
     }
   }
 }
